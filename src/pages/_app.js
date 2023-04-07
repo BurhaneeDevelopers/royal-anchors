0bf1038c6040905d8dcf930989c18ce00869fd9c
Mohammed <86866415+BurhaneeDevelopers@users.noreply.github.com>
2023-04-07 17:17:06 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { useEffect } from "react";
+import AOS from "aos";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const use = async () => {
+      (await import("tw-elements")).default;
+    };
+    use();
+  }, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+    AOS.refresh();
+  }, []);
+  return <Component {...pageProps} />;
 }
