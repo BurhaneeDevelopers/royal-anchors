fef68ed4c435bb3599421b88ebd10f90eccdfe43
Mohammed <86866415+BurhaneeDevelopers@users.noreply.github.com>
2023-04-09 09:27:40 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
