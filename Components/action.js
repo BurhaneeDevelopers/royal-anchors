36a449885fc8e89cea362344cca710e2a286d332
Mohammed <86866415+BurhaneeDevelopers@users.noreply.github.com>
2023-04-09 09:18:29 +0530

    changes

--- Components/action.js
+++ Components/action.js
@@ -15,20 +15,20 @@ const Action = () => {
       {isVisible && (
         <div className="">
           <div
-            className="fixed bg-[#49E670] p-3 left-10 bottom-40 z-50 rounded-full transition-all duration-500 cursor-pointer tooltip tooltip-right tooltip-white"
+            className="fixed bg-[#49E670] p-3 left-10 bottom-40 z-50 rounded-full transition-all duration-500 cursor-pointer tooltip tooltip-right"
             data-tip="Whatsapp"
           >
             <BsWhatsapp className="text-white text-3xl font-bold" />
           </div>
           {/* <ToolTip text="This is a tooltip!"> */}
-            <div className="fixed bg-[#03E78B] p-3 left-10 bottom-[100px] z-50 rounded-full transition-all duration-500 cursor-pointer">
+            <div className="fixed bg-[#03E78B] p-3 left-10 bottom-[100px] z-50 rounded-full transition-all duration-500 cursor-pointer tooltip tooltip-right" data-tip="Whatsapp">
               <FaPhoneAlt className="text-white text-3xl font-bold" />
             </div>
           {/* </ToolTip> */}
         </div>
       )}
       <div
-        className="fixed bg-[#A886CD] p-3 flex justify-center items-center text-center left-10 bottom-10 z-50 rounded-full transition-all duration-500 cursor-pointer tooltip tooltip-right tooltip-white"
+        className="fixed bg-[#A886CD] p-3 flex justify-center items-center text-center left-10 bottom-10 z-50 rounded-full transition-all duration-500 cursor-pointer tooltip tooltip-right"
         data-tip="Open/Hide"
         onClick={handleButtonClick}
       >
